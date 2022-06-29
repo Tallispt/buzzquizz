6020335de30ae7d0d799eb094ecc54613fcb6f08
Tallis <tallispt@gmail.com>
2022-06-29 11:36:31 -0300

    Listagem de todos os quizzes

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 //TELA 1
+function listaQuizz() {
+    let promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+    promise
+        .catch(err => console.log(err))
 
+        .then(result => {
+            document.querySelector('.feed').innerHTML =
+                `<div class="quizzes-container">
+                <div class="title">Todos os Quizzes</div>
+                    <div class="quizzes">
+                </div>
+            </div>`
 
+            let quizzIndividual = document.querySelector('.quizzes')
+            for (let i = 0; i < result.data.length; i++) {
+                quizzIndividual.innerHTML +=
+                    `<div class="individual-quizz" onclick="abrirQuizz(this)">
+                        <img src="${result.data[i].image}" class="cover">
+                        <div class="gradient"></div>
+                        <p>${result.data[i].title}</p>
+                    </div>`
+            }
+        })
+}
 
+function abrirQuizz(elemento) {
+    document.querySelector('.feed').innerHTML = `<button onclick='listaQuizz()'>Voltar</button>`
+}
+
+listaQuizz();
 
 
 
