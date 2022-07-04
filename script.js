13cf28394f05d0d6552493f7a473d6a5dba6a0a3
Tallis <tallispt@gmail.com>
2022-07-04 01:12:55 -0300

    algumas adiçoes

--- script.js
+++ script.js
@@ -269,7 +269,6 @@ let regex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
 let verificaUrl = /^[a-zA-Z0-9-_]+[:./\\]+([a-zA-Z0-9 -_./:=&"'?%+@#$!])+$/
 
 function criarQuiz() {
-  loading()
 
   document.querySelector('.titulo-pagina').innerHTML =
     '<h1>Comece pelo começo</h1>'
